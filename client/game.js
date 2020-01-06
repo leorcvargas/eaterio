237c5bd2e5dbdede8e1610803bc17da633c05cca
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2020-01-06 20:24:56 -0300

    Refactoring moves

--- client/game.js
+++ client/game.js
@@ -47,24 +47,32 @@ const createGame = () => {
   const movePlayer = ({ playerId, keyPressed }) => {
     const player = state.players[playerId];
 
-    if (keyPressed === "ArrowUp" && player.y - 1 >= 0) {
-      player.y -= 1;
-      return;
-    }
+    const moves = {
+      ArrowUp: () => {
+        if (player.y - 1 >= 0) {
+          player.y -= 1;
+        }
+      },
+      ArrowDown: () => {
+        if (player.y + 1 < screen.height) {
+          player.y += 1;
+        }
+      },
+      ArrowLeft: () => {
+        if (player.x - 1 >= 0) {
+          player.x -= 1;
+        }
+      },
+      ArrowRight: () => {
+        if (player.x + 1 < screen.width) {
+          player.x += 1;
+        }
+      }
+    };
 
-    if (keyPressed === "ArrowDown" && player.y + 1 < screen.height) {
-      player.y += 1;
-      return;
-    }
-
-    if (keyPressed === "ArrowRight" && player.x + 1 < screen.width) {
-      player.x += 1;
-      return;
-    }
-
-    if (keyPressed === "ArrowLeft" && player.x - 1 >= 0) {
-      player.x -= 1;
-      return;
+    const moveFunction = moves[keyPressed];
+    if (moveFunction) {
+      moveFunction();
     }
   };
 
