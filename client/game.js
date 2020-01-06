7d02bf48b3f9d4a72c6ab09bdd4f9ffc42914bb1
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2020-01-06 20:10:38 -0300

    first commit

---
+++ client/game.js
@@ -0,0 +1,106 @@
+const currentPlayerId = "player1";
+const screen = document.getElementById("screen");
+const context = screen.getContext("2d");
+
+const addPlayer = (context, { x, y }) => {
+  const color = "green";
+
+  context.fillStyle = color;
+  context.fillRect(x, y, 1, 1);
+};
+
+const addFruit = (context, { x, y }) => {
+  const color = "red";
+
+  context.fillStyle = color;
+  context.fillRect(x, y, 1, 1);
+};
+
+const render = (context, state) => {
+  context.clearRect(0, 0, 500, 500);
+
+  for (const playerId in state.players) {
+    const player = state.players[playerId];
+    addPlayer(context, player);
+  }
+
+  for (const fruitId in state.fruits) {
+    const fruit = state.fruits[fruitId];
+    addFruit(context, fruit);
+  }
+
+  requestAnimationFrame(() => render(context, state));
+};
+
+const createGame = () => {
+  const state = {
+    players: {
+      player1: { x: 1, y: 1 },
+      player2: { x: 9, y: 2 },
+      player3: { x: 5, y: 8 }
+    },
+    fruits: {
+      fruit1: { x: 3, y: 9 }
+    }
+  };
+
+  const movePlayer = ({ playerId, keyPressed }) => {
+    const player = state.players[playerId];
+
+    if (keyPressed === "ArrowUp" && player.y - 1 >= 0) {
+      player.y -= 1;
+      return;
+    }
+
+    if (keyPressed === "ArrowDown" && player.y + 1 < screen.height) {
+      player.y += 1;
+      return;
+    }
+
+    if (keyPressed === "ArrowRight" && player.x + 1 < screen.width) {
+      player.x += 1;
+      return;
+    }
+
+    if (keyPressed === "ArrowLeft" && player.x - 1 >= 0) {
+      player.x -= 1;
+      return;
+    }
+  };
+
+  return {
+    state,
+    movePlayer
+  };
+};
+
+const createKeyboardListener = () => {
+  const state = {
+    observers: []
+  };
+
+  const subscribe = observerFunction => {
+    state.observers.push(observerFunction);
+  };
+
+  const notifyAll = command => {
+    for (const observerFunction of state.observers) {
+      observerFunction(command);
+    }
+  };
+
+  const handleKeyDown = event =>
+    notifyAll({ playerId: "player1", keyPressed: event.key });
+
+  document.addEventListener("keydown", handleKeyDown);
+
+  return {
+    subscribe
+  };
+};
+
+const game = createGame();
+const keyboardListener = createKeyboardListener();
+keyboardListener.subscribe(game.movePlayer);
+
+render(context, game.state);
