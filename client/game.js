b3c17cbc450bc74a4e93eece52158e8c3f2449aa
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2020-01-06 20:52:45 -0300

    Fruit collision

--- client/game.js
+++ client/game.js
@@ -2,14 +2,14 @@ const currentPlayerId = "player1";
 const screen = document.getElementById("screen");
 const context = screen.getContext("2d");
 
-const addPlayer = (context, { x, y }) => {
-  const color = "green";
+const renderPlayer = (context, { x, y }) => {
+  const color = "gray";
 
   context.fillStyle = color;
   context.fillRect(x, y, 1, 1);
 };
 
-const addFruit = (context, { x, y }) => {
+const renderFruit = (context, { x, y }) => {
   const color = "red";
 
   context.fillStyle = color;
@@ -21,12 +21,12 @@ const render = (context, state) => {
 
   for (const playerId in state.players) {
     const player = state.players[playerId];
-    addPlayer(context, player);
+    renderPlayer(context, player);
   }
 
   for (const fruitId in state.fruits) {
     const fruit = state.fruits[fruitId];
-    addFruit(context, fruit);
+    renderFruit(context, fruit);
   }
 
   requestAnimationFrame(() => render(context, state));
@@ -34,13 +34,35 @@ const render = (context, state) => {
 
 const createGame = () => {
   const state = {
-    players: {
-      player1: { x: 1, y: 1 },
-      player2: { x: 9, y: 2 },
-      player3: { x: 5, y: 8 }
-    },
-    fruits: {
-      fruit1: { x: 3, y: 9 }
+    players: {},
+    fruits: {}
+  };
+
+  const addPlayer = ({ playerId, x, y }) => {
+    state.players[playerId] = { x, y };
+  };
+
+  const removePlayer = ({ playerId }) => {
+    delete state.players[playerId];
+  };
+
+  const addFruit = ({ fruitId, x, y }) => {
+    state.fruits[fruitId] = { x, y };
+  };
+
+  const removeFruit = ({ fruitId }) => {
+    delete state.fruits[fruitId];
+  };
+
+  const checkForFruitCollision = playerId => {
+    const player = state.players[playerId];
+
+    for (const fruitId in state.fruits) {
+      const fruit = state.fruits[fruitId];
+
+      if (fruit.x === player.x && fruit.y === player.y) {
+        removeFruit({ fruitId });
+      }
     }
   };
 
@@ -71,14 +93,20 @@ const createGame = () => {
     };
 
     const moveFunction = moves[keyPressed];
-    if (moveFunction) {
+
+    if (player && moveFunction) {
       moveFunction();
+      checkForFruitCollision(playerId);
     }
   };
 
   return {
-    state,
-    movePlayer
+    movePlayer,
+    addPlayer,
+    removePlayer,
+    addFruit,
+    removeFruit,
+    state
   };
 };
 
